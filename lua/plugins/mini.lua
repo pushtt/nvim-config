16a8d53e756d8a548a419070d49c5ce2386c35d3
Thinh Tran <thinhphuctran@gmail.com>
2025-10-04 10:50:56 +0200

    add start up screen

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,27 @@
 return {
-        "echasnovski/mini.nvim",
-        config = function()
-            require("mini.ai").setup()
-            require("mini.statusline").setup{use_icons = true}
-        end,
+    "echasnovski/mini.nvim",
+    config = function()
+        require("mini.ai").setup()
+
+        require("mini.statusline").setup{use_icons = true}
+        local animate = require("mini.animate")
+        animate.setup{
+            scroll = {
+                enable = false
+            },
+            cursor = {
+                timing = animate.gen_timing.linear({ duration = 100, unit = 'total' })
+            }
+        }
+
+        require("mini.starter").setup{
+            header = [[███████╗ ██████╗ ██╗   ██╗██╗      ██████╗ ███████╗███████╗██╗███╗   ██╗███████╗
+██╔════╝██╔═══██╗██║   ██║██║      ██╔══██╗██╔════╝██╔════╝██║████╗  ██║██╔════╝
+███████╗██║   ██║██║   ██║██║      ██████╔╝█████╗  █████╗  ██║██╔██╗ ██║█████╗
+╚════██║██║   ██║██║   ██║██║      ██╔══██╗██╔══╝  ██╔══╝  ██║██║╚██╗██║██╔══╝
+███████║╚██████╔╝╚██████╔╝███████╗ ██║  ██║███████╗██║     ██║██║ ╚████║███████╗
+╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝ ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚═╝  ╚═══╝╚══════╝
+]]
+        }
+    end,
 }
