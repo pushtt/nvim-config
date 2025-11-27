d84ba7fe2d2b190f07a59eb68ab44632a165cd86
Thinh Tran <thinhphuctran@gmail.com>
2025-11-27 20:43:03 +0100

    add R plugin

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,96 +1,96 @@
 return {
-    'saghen/blink.cmp',
-    dependencies = {
-        'rafamadriz/friendly-snippets',
-        'onsails/lspkind.nvim'
-    },
-    version = '1.*',
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = { preset = 'default' },
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
+	},
+	version = "1.*",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = { preset = "default" },
 
-        appearance = {
-            nerd_font_variant = 'mono',
-        },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-        snippets = {
-            preset = 'luasnip',
-        },
+		snippets = {
+			preset = "luasnip",
+		},
 
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-            providers = {
-                snippets = {
-                    name = 'snippets',
-                    module = 'blink.cmp.sources.snippets',
-                    enabled = true,
-                    opts = {
-                        luasnip = {
-                            -- Enable LuaSnip integration
-                        }
-                    }
-                },
-                lazydev = {
-                    name = "LazyDev",
-                    module = "lazydev.integrations.blink",
-                    -- make lazydev completions top priority (see `:h blink.cmp`)
-                    score_offset = 100,
-                },
-            },
-        },
-        fuzzy = { implementation = "prefer_rust_with_warning" },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			providers = {
+				snippets = {
+					name = "snippets",
+					module = "blink.cmp.sources.snippets",
+					enabled = true,
+					opts = {
+						luasnip = {
+							-- Enable LuaSnip integration
+						},
+					},
+				},
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
+			},
+		},
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 
-        completion = {
+		completion = {
 
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 250,
-                treesitter_highlighting = true,
-                window = { border = "rounded" },
-            },
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 250,
+				treesitter_highlighting = true,
+				window = { border = "rounded" },
+			},
 
-            menu = {
-                border = "rounded",
+			menu = {
+				border = "rounded",
 
-                cmdline_position = function()
-                    if vim.g.ui_cmdline_pos ~= nil then
-                        local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
-                        return { pos[1] - 1, pos[2] }
-                    end
-                    local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
-                    return { vim.o.lines - height, 0 }
-                end,
+				cmdline_position = function()
+					if vim.g.ui_cmdline_pos ~= nil then
+						local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+						return { pos[1] - 1, pos[2] }
+					end
+					local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+					return { vim.o.lines - height, 0 }
+				end,
 
-                draw = {
-                    columns = {
-                        { "kind_icon", "label", gap = 1 },
-                        { "kind" },
-                    },
-                    components = {
-                        kind_icon = {
-                            text = function(item)
-                                local kind = require("lspkind").symbol_map[item.kind] or ""
-                                return kind .. " "
-                            end,
-                            highlight = "CmpItemKind",
-                        },
-                        label = {
-                            text = function(item)
-                                return item.label
-                            end,
-                            highlight = "CmpItemAbbr",
-                        },
-                        kind = {
-                            text = function(item)
-                                return item.kind
-                            end,
-                            highlight = "CmpItemKind",
-                        },
-                    },
-                },
-            },
-        },
-    },
-    opts_extend = { "sources.default" }
+				draw = {
+					columns = {
+						{ "kind_icon", "label", gap = 1 },
+						{ "kind" },
+					},
+					components = {
+						kind_icon = {
+							text = function(item)
+								local kind = require("lspkind").symbol_map[item.kind] or ""
+								return kind .. " "
+							end,
+							highlight = "CmpItemKind",
+						},
+						label = {
+							text = function(item)
+								return item.label
+							end,
+							highlight = "CmpItemAbbr",
+						},
+						kind = {
+							text = function(item)
+								return item.kind
+							end,
+							highlight = "CmpItemKind",
+						},
+					},
+				},
+			},
+		},
+	},
+	opts_extend = { "sources.default" },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,222 +1,221 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-                -- used for completion, annotations and signatures of Neovim apis
-                "folke/lazydev.nvim",
-                ft = "lua",
-                opts = {
-                    library = {
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "luvit-meta/library", words = { "vim%.uv" } },
-                        { path = "/usr/share/awesome/lib/", words = { "awesome" } },
-                    },
-                },
-            },
-            { "Bilal2453/luvit-meta", lazy = true },
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+				-- used for completion, annotations and signatures of Neovim apis
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "luvit-meta/library", words = { "vim%.uv" } },
+						{ path = "/usr/share/awesome/lib/", words = { "awesome" } },
+					},
+				},
+			},
+			{ "Bilal2453/luvit-meta", lazy = true },
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-            { "j-hui/fidget.nvim", opts = {} },
-            { "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
+			{ "j-hui/fidget.nvim", opts = {} },
+			{ "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
 
-            -- Autoformatting
-            "stevearc/conform.nvim",
+			-- Autoformatting
+			"stevearc/conform.nvim",
 
-            -- Schema information
-            "b0o/SchemaStore.nvim",
-        },
-        config = function()
-            local capabilities = nil
-            if pcall(require, "cmp_nvim_lsp") then
-                capabilities = require("cmp_nvim_lsp").default_capabilities()
-            end
+			-- Schema information
+			"b0o/SchemaStore.nvim",
+		},
+		config = function()
+			local capabilities = nil
+			if pcall(require, "blink.cmp") then
+				capabilities = require("blink.cmp").get_lsp_capabilities()
+			end
 
-            local servers = {
-                bashls = true,
-                sqlls = true,
-                gopls = {
-                    manual_install = true,
-                    settings = {
-                        gopls = {
-                            hints = {
-                                assignVariableTypes = true,
-                                compositeLiteralFields = true,
-                                compositeLiteralTypes = true,
-                                constantValues = true,
-                                functionTypeParameters = true,
-                                parameterNames = true,
-                                rangeVariableTypes = true,
-                            },
-                        },
-                    },
-                },
-                glsl_analyzer = true,
-                lua_ls = {
-                    cmd = { "lua-language-server" },
-                    settings = {
-                        Lua = {
-                            diagnostics = { disable = { 'missing-fields' } }
-                        }
-                    }
-                    -- server_capabilities = {
-                        --   semanticTokensProvider = vim.NIL,
-                        -- },
-                    },
-                    rust_analyzer = true,
-                    svelte = true,
-                    templ = true,
-                    taplo = true,
-                    intelephense = {
-                        settings = {
-                            intelephense = {
-                                format = {
-                                    braces = "k&r",
-                                },
-                            },
-                        },
-                    },
+			local servers = {
+				air = true,
+				bashls = true,
+				sqlls = true,
+				gopls = {
+					manual_install = true,
+					settings = {
+						gopls = {
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+						},
+					},
+				},
+				glsl_analyzer = true,
+				lua_ls = {
+					cmd = { "lua-language-server" },
+					settings = {
+						Lua = {
+							diagnostics = { disable = { "missing-fields" } },
+						},
+					},
+					-- server_capabilities = {
+					--   semanticTokensProvider = vim.NIL,
+					-- },
+				},
+				rust_analyzer = true,
+				svelte = true,
+				templ = true,
+				taplo = true,
+				intelephense = {
+					settings = {
+						intelephense = {
+							format = {
+								braces = "k&r",
+							},
+						},
+					},
+				},
 
-                    pyright = true,
-                    ruff = { manual_install = true },
-                    -- mojo = { manual_install = true },
+				pyright = true,
+				ruff = { manual_install = true },
+				-- mojo = { manual_install = true },
 
-                    -- Enabled biome formatting, turn off all the other ones generally
-                    biome = true,
-                    astro = true,
-                    vtsls = {
-                        server_capabilities = {
-                            documentFormattingProvider = false,
-                        },
-                    },
-                    -- denols = true,
-                    jsonls = {
-                        server_capabilities = {
-                            documentFormattingProvider = false,
-                        },
-                        settings = {
-                            json = {
-                                schemas = require("schemastore").json.schemas(),
-                                validate = { enable = true },
-                            },
-                        },
-                    },
-                    yamlls = {
-                        settings = {
-                            yaml = {
-                                schemaStore = {
-                                    enable = false,
-                                    url = "",
-                                },
-                                -- schemas = require("schemastore").yaml.schemas(),
-                            },
-                        },
-                    },
+				-- Enabled biome formatting, turn off all the other ones generally
+				biome = true,
+				astro = true,
+				vtsls = {
+					server_capabilities = {
+						documentFormattingProvider = false,
+					},
+				},
+				-- denols = true,
+				jsonls = {
+					server_capabilities = {
+						documentFormattingProvider = false,
+					},
+					settings = {
+						json = {
+							schemas = require("schemastore").json.schemas(),
+							validate = { enable = true },
+						},
+					},
+				},
+				yamlls = {
+					settings = {
+						yaml = {
+							schemaStore = {
+								enable = false,
+								url = "",
+							},
+							-- schemas = require("schemastore").yaml.schemas(),
+						},
+					},
+				},
+				clangd = {
+					-- cmd = { "clangd", unpack(require("custom.clangd").flags) },
+					-- TODO: Could include cmd, but not sure those were all relevant flags.
+					--    looks like something i would have added while i was floundering
+					init_options = { clangdFileStatus = true },
 
-                    clangd = {
-                        -- cmd = { "clangd", unpack(require("custom.clangd").flags) },
-                        -- TODO: Could include cmd, but not sure those were all relevant flags.
-                        --    looks like something i would have added while i was floundering
-                        init_options = { clangdFileStatus = true },
+					filetypes = { "c" },
+				},
+			}
 
-                        filetypes = { "c" },
-                    },
+			local servers_to_install = vim.tbl_filter(function(key)
+				local t = servers[key]
+				if type(t) == "table" then
+					return not t.manual_install
+				else
+					return t
+				end
+			end, vim.tbl_keys(servers))
 
-                }
+			require("mason").setup()
+			local ensure_installed = {
+				"pyright",
+				"gopls",
+			}
 
-                local servers_to_install = vim.tbl_filter(function(key)
-                    local t = servers[key]
-                    if type(t) == "table" then
-                        return not t.manual_install
-                    else
-                        return t
-                    end
-                end, vim.tbl_keys(servers))
+			vim.list_extend(ensure_installed, servers_to_install)
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-                require("mason").setup()
-                local ensure_installed = {
-                    "pyright",
-                    "gopls"
-                }
+			for name, config in pairs(servers) do
+				if config == true then
+					config = {}
+				end
+				config = vim.tbl_deep_extend("force", {}, {
+					capabilities = capabilities,
+				}, config)
 
-                vim.list_extend(ensure_installed, servers_to_install)
-                require("mason-tool-installer").setup { ensure_installed = ensure_installed }
+				vim.lsp.config(name, config)
+				vim.lsp.enable(name, true)
+			end
 
-                for name, config in pairs(servers) do
-                    if config == true then
-                        config = {}
-                    end
-                    config = vim.tbl_deep_extend("force", {}, {
-                        capabilities = capabilities,
-                    }, config)
+			local disable_semantic_tokens = {
+				-- lua = true,
+			}
 
-                    vim.lsp.config(name, config)
-                    vim.lsp.enable(name, true)
-                end
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local bufnr = args.buf
+					local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
 
-                local disable_semantic_tokens = {
-                    -- lua = true,
-                }
+					local settings = servers[client.name]
+					if type(settings) ~= "table" then
+						settings = {}
+					end
 
-                vim.api.nvim_create_autocmd("LspAttach", {
-                    callback = function(args)
-                        local bufnr = args.buf
-                        local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+					local builtin = require("telescope.builtin")
 
-                        local settings = servers[client.name]
-                        if type(settings) ~= "table" then
-                            settings = {}
-                        end
+					vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+					vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+					vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+					vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
 
-                        local builtin = require "telescope.builtin"
+					vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = 0 })
+					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
+					vim.keymap.set("n", "<leader>wd", builtin.lsp_document_symbols, { buffer = 0 })
+					vim.keymap.set("n", "<leader>ww", function()
+						builtin.diagnostics({ root_dir = true })
+					end, { buffer = 0 })
 
-                        vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-                        vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
-                        vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
-                        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-                        vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
-                        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+					local filetype = vim.bo[bufnr].filetype
+					if disable_semantic_tokens[filetype] then
+						client.server_capabilities.semanticTokensProvider = nil
+					end
 
-                        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = 0 })
-                        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = 0 })
-                        vim.keymap.set("n", "<leader>wd", builtin.lsp_document_symbols, { buffer = 0 })
-                        vim.keymap.set("n", "<leader>ww", function()
-                            builtin.diagnostics { root_dir = true }
-                        end, { buffer = 0 })
+					-- Override server capabilities
+					if settings.server_capabilities then
+						for k, v in pairs(settings.server_capabilities) do
+							if v == vim.NIL then
+								---@diagnostic disable-next-line: cast-local-type
+								v = nil
+							end
 
-                        local filetype = vim.bo[bufnr].filetype
-                        if disable_semantic_tokens[filetype] then
-                            client.server_capabilities.semanticTokensProvider = nil
-                        end
+							client.server_capabilities[k] = v
+						end
+					end
+				end,
+			})
 
-                        -- Override server capabilities
-                        if settings.server_capabilities then
-                            for k, v in pairs(settings.server_capabilities) do
-                                if v == vim.NIL then
-                                    ---@diagnostic disable-next-line: cast-local-type
-                                    v = nil
-                                end
+			require("lsp_lines").setup()
+			vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
 
-                                client.server_capabilities[k] = v
-                            end
-                        end
-                    end,
-                })
-
-                require("lsp_lines").setup()
-                vim.diagnostic.config { virtual_text = true, virtual_lines = false }
-
-                vim.keymap.set("", "<leader>l", function()
-                    local config = vim.diagnostic.config() or {}
-                    if config.virtual_text then
-                        vim.diagnostic.config { virtual_text = false, virtual_lines = true }
-                    else
-                        vim.diagnostic.config { virtual_text = true, virtual_lines = false }
-                    end
-                end, { desc = "Toggle lsp_lines" })
-            end,
-        },
-    }
+			vim.keymap.set("", "<leader>l", function()
+				local config = vim.diagnostic.config() or {}
+				if config.virtual_text then
+					vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
+				else
+					vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
+				end
+			end, { desc = "Toggle lsp_lines" })
+		end,
+	},
+}
